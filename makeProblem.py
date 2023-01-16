d48787f3b4558163217beb5e9a5ac6b9a17e39d7
Alan Bui <alanb3350@gmail.com>
2023-01-15 19:15:37 -0500

    added new problem and solution

--- makeProblem.py
+++ makeProblem.py
@@ -1,13 +1,13 @@
 from datetime import date
 
-PAGETITLE = "Fermat 2005 Q20"
+PAGETITLE = "Fermat 2000 Q17"
 AUTHOR = "Alan_Bui"
 DATE = str(date.today())
 CATEGORIES = ["Fermat"]
 TAGS = ["geometry", "TO DO"]
 
 FILENAME = "_posts/"+DATE+"-"+PAGETITLE.replace(" ", "-")+".md"
-PROBLEMLINK = "https://cemc.uwaterloo.ca/contests/past_contests/2005/2005FermatContest.pdf"
+PROBLEMLINK = "https://cemc.uwaterloo.ca/contests/past_contests/2000/2000FermatContest.pdf"
 
 cats = str(CATEGORIES).replace("'", '')
 tags = str(TAGS).replace("'", '')
@@ -26,9 +26,13 @@ mermaid: true
 ---
 ## Problem Statement
 
-In triangle $ABC$, if $AB = AC = x + 1$ and $BC = 2x - 2$, where $x > 1$, then the area of the triangle is always equal to
+Three circles have centres A, B and C with radii 2, 4 and 6 respectively. The circles are tangent to each other as shown. $\\triangle ABC$ has
+
+(A) $\\angle A$ obtuse (B) $\\angle B = 90^{\\circ}$ (C) $angle A = 90^{\circ}$ (D) all angles acute (E) $\\angle A = \\angleB$
 
 [Problem Link]('''+PROBLEMLINK+''')
+
+![Problem Diagram](/assets/diagrams/fermat2000q17.png)
 
 ## Solution
 
