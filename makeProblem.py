be87f5a7819923b18bd90f18ea977d1b01f05460
Alan Bui <alanb3350@gmail.com>
2023-02-18 11:48:04 -0500

    added problems

--- makeProblem.py
+++ makeProblem.py
@@ -1,13 +1,13 @@
 from datetime import date
 
-PAGETITLE = "Fermat 1998 Q22"
+PAGETITLE = "Fermat 2009 Q23"
 AUTHOR = "Alan_Bui"
 DATE = str(date.today())
 CATEGORIES = ["Fermat"]
 TAGS = ["number theory"]
 
 FILENAME = "_posts/"+DATE+"-"+PAGETITLE.replace(" ", "-")+".md"
-PROBLEMLINK = "https://cemc.uwaterloo.ca/contests/past_contests/1998/1998FermatContest.pdf"
+PROBLEMLINK = "https://cemc.uwaterloo.ca/contests/past_contests/2009/2009FermatContest.pdf"
 
 cats = str(CATEGORIES).replace("'", '')
 tags = str(TAGS).replace("'", '')
@@ -28,7 +28,15 @@ mermaid: true
 
 [Problem Link]('''+PROBLEMLINK+''')
 
-There are four unequal, positive integers $a$, $b$, $c$, and $N$ such that $N = 5a + 3b + 5c$. It is also true that $N = 4a + 5b + 4c$ and $N is between 131 and 150. What is the value of $a+b+c$ ?
+Suppose that $a$, $b$, $c$, and $d$ are positive integers that satisfy the equations
+
+$$ab + cd = 38$$
+
+$$ac + bd = 34$$
+
+$$ad + bc = 43$$
+
+What is the value of $a$ + $b$ + $c$ + $d$?
 
 ## Solution
 
