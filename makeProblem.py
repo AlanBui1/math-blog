287d1c0ae96397ce9fc54a0565fe39b6187e7744
Alan Bui <alanb3350@gmail.com>
2023-02-21 00:03:49 -0500

    added new posts

--- makeProblem.py
+++ makeProblem.py
@@ -1,13 +1,13 @@
 from datetime import date
 
-PAGETITLE = "AIME II 2006 P7"
+PAGETITLE = "AIME 1996 P12"
 AUTHOR = "Alan_Bui"
 DATE = str(date.today())
 CATEGORIES = ["AIME"]
 TAGS = ["combo", "TO DO"]
 
 FILENAME = "_posts/"+DATE+"-"+PAGETITLE.replace(" ", "-")+".md"
-PROBLEMLINK = "https://artofproblemsolving.com/wiki/index.php/2006_AIME_II_Problems/Problem_7"
+PROBLEMLINK = "https://artofproblemsolving.com/wiki/index.php/1996_AIME_Problems/Problem_12"
 
 cats = str(CATEGORIES).replace("'", '')
 tags = str(TAGS).replace("'", '')
@@ -26,9 +26,13 @@ mermaid: true
 ---
 ## Problem Statement
 
-[Problem Link]('''+PROBLEMLINK+''')
+For each permutation $a_1,a_2,a_3,\cdots,a_{10}$ of the integers $1,2,3,\cdots,10$, form the sum
 
-Find the number of ordered pairs of positive integers $(a,b)$ such that $a+b=1000$ and neither $a$ nor $b$ has a zero digit.
+\[|a_1-a_2|+|a_3-a_4|+|a_5-a_6|+|a_7-a_8|+|a_9-a_{10}|.\]
+
+The average value of all such sums can be written in the form $\dfrac{p}{q}$, where $p$ and $q$ are relatively prime positive integers. Find $p+q$.
+
+[Problem Link]('''+PROBLEMLINK+''')
 
 ## Solution
 
